a5875bb82806c8ee5ffa61ca753daafefce4b3cb
Stas Jng <sonthth1903012@fpt.edu.vn>
2021-04-23 08:45:23 +0700

    update

---
+++ rest_test01/src/main/java/com/example/rest02/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.example.rest02.repository;
+
+import com.example.rest02.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+
+@RepositoryRestResource
+public interface BookRepository extends JpaRepository<Book, String> {
+}
---
+++ rest_test01/src/main/java/com/example/rest02/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.example.rest02.repository;
+
+import com.example.rest02.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+}
